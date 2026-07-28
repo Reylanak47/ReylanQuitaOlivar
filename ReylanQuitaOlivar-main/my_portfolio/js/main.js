b3b05411c38a8f43b6ab749b59e9f8459d07a526
Reylanak47 <137566997+Reylanak47@users.noreply.github.com>
2026-07-28 19:15:42 +0800

    Delete ReylanQuitaOlivar-main directory

--- ReylanQuitaOlivar-main/my_portfolio/js/main.js
+++
@@ -1,7 +0,0 @@
-// Set footer year automatically
-const yearSpan = document.getElementById("year");
-if (yearSpan) {
-  yearSpan.textContent = new Date().getFullYear();
-}
-
-console.log("Portfolio loaded: Reylan | Student Developer & Builder");
